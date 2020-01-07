b8243c0e8dc338a1220efaf1198bc57c880f4a76
Pekcapeh <58980761+Pekcapeh@users.noreply.github.com>
2020-01-07 12:34:07 +0700

    Исправил отображение Panel

--- Assets/Scripts/ButtonShowCreator.cs
+++ Assets/Scripts/ButtonShowCreator.cs
@@ -4,18 +4,24 @@ public class ButtonShowCreator : MonoBehaviour
 {      
     public GameObject Panel;
 
-    private bool _show = false;    
+    private float _width;
+    private bool _show = false;
+
+    private void Start()
+    {
+        _width = Panel.GetComponent<RectTransform>().sizeDelta.x;
+    }
 
     public void ShowCreator()
     {
         if (_show == false)
         {
-            Panel.transform.Translate(-200, 0, 0);
+            Panel.transform.Translate(- _width, 0, 0);
             _show = true;
         }
         else
         {
-            Panel.transform.Translate(200, 0, 0);
+            Panel.transform.Translate(_width, 0, 0);
             _show = false;
         }
     }
